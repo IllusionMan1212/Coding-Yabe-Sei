9d4cbfdcd04e1356df40affad41e0d27c2253b7a
ThatCopy <62877244+ThatCopy@users.noreply.github.com>
2020-10-17 19:48:41 +0200

    fix the fucksy wucksie!!!

--- commands/fun/owofy.js
+++ commands/fun/owofy.js
@@ -1,19 +1,14 @@
 const Discord = require('discord.js');
+const owo = require('owofy');
 
 exports.run = (client, message, args) => {
     let sentence = args.join(' ');
     if (!sentence) return message.reply('I can\'t owo-fy an empty message! uwu');
-    
-    let faces=["(・`ω´・)",";;w;;","owo","UwU",">w<","^w^"];
 
-    let newSentence = sentence.replace(/[lr]/g, 'w');
-      newSentence = newSentence.replace(/(?:r|l)/g, "w");
-      newSentence = newSentence.replace(/(?:R|L)/g, "W");
-      newSentence = newSentence.replace(/n([aeiou])/g, 'ny$1');
-      newSentence = newSentence.replace(/N([aeiou])/g, 'Ny$1');
-      newSentence = newSentence.replace(/N([AEIOU])/g, 'NY$1');
-      newSentence = newSentence.replace(/ove/g, "uv");
-      newSentence = newSentence.replace(/\!+/g, " "+ faces[Math.floor(Math.random()*faces.length)]+ " ");
+    let cleanSentence = sentence.replace('@', ' ');
+
+    let newSentence = owo(cleanSentence);
+
     // fuck you this is now finished
     message.channel.send(newSentence);
 }
