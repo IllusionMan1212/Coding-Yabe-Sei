172ca52dbb8745c298fccf9259e7a24b36b0ffae
IllusionMan1212 <hisham.abourgheba@gmail.com>
2020-11-12 17:31:07 +0200

    add days for uptime command

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -3,13 +3,14 @@ exports.run = (client, message, args) => {
     var milliseconds = parseInt((client.uptime % 1000) / 100),
         seconds = parseInt((client.uptime / 1000) % 60),
         minutes = parseInt((client.uptime / (1000 * 60)) % 60),
-        hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24);
+        hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24),
+        days = parseInt((client.uptime / (1000 * 60 * 60 * 24)) % 1);
 
     hours = (hours < 10) ? "0" + hours : hours;
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-    message.channel.send(":chart_with_upwards_trend: I've been running for** " + hours + " **hours, **" + minutes + "** minutes and **" + seconds + "." + milliseconds + "** seconds!");
+    message.channel.send(":chart_with_upwards_trend: I've been running for** " + days + " **days, **" + hours + " **hours, **" + minutes + "** minutes and **" + seconds + "." + milliseconds + "** seconds!");
 }
 
 exports.help = {
